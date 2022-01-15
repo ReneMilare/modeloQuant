94b3bbca938ce7b14e3a1e4a845e5c6f1e8e10f1
ReneMilare <milarerp@gmail.com>
2022-01-15 10:39:40 -0300

    first commit

---
+++ auxs.py
@@ -0,0 +1,406 @@
+import numpy as np
+import talib
+import random
+import tensorflow as tf
+import keras
+import itertools
+import pandas as pd
+from tensorflow.keras.optimizers import Adam
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import BaggingClassifier
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import StackingClassifier
+from sklearn.multiclass import OneVsOneClassifier
+import MetaTrader5 as mt5
+import math
+
+def add_lags(data, lags, window=20, fit_model = True):
+  cols = []
+  df = data.copy()
+  df['retorno'] = np.log(df.close / df.close.shift())
+  df['min'] = df.close.rolling(window).min()
+  df['max'] = df.close.rolling(window).max()
+  df['mom'] = df.retorno.rolling(window).mean()
+  df['vol'] = df.retorno.rolling(window).std()
+
+  features = [
+    'volatilidade',
+    'std_9',
+    'std_20',
+    'distancia_mean_9',
+    'distancia_mean_20',
+    'corpo_candle',
+    'distancia_medias',
+    'V_Max',
+    'V_Min',
+    'I',
+  ]
+
+  beta_periods = [100, 150, 200]
+
+  for beta in beta_periods:
+    combinations = list(itertools.permutations(['open', 'high', 'low', 'close'],2))
+    for combination in combinations:
+      col = f'beta_{combination[0]}_{combination[1]}_{beta}'
+      df[col] = talib.BETA(df[combination[0]], df[combination[1]], timeperiod=beta)
+      df[col] = pd.qcut(df[col], 5, labels=False)
+      features.append(col)
+      cols.append(col)
+
+  df['mma20'] = talib.MA(df.close, timeperiod=20)
+  df['mme9'] = talib.EMA(df.close, timeperiod=9)
+
+  df['alta_ou_baixa'] = np.where(df.open <= df.close, 1, 0)
+  if fit_model:
+    df['alvo'] = df.alta_ou_baixa.shift(-1)
+  df['mean_9'] = df.close.rolling(9).mean()
+  df['mean_20'] = df.close.rolling(20).mean()
+
+  df['volatilidade'] = ((df.high - df.low)/df.low) * 100
+  df['std_9'] = df.close.rolling(9).std()*100
+  df['std_20'] = df.close.rolling(20).std()*100
+  df['distancia_mean_9'] = ((df.close - df.mean_9)/df.mean_9) * 100
+  df['distancia_mean_20'] = ((df.close - df.mean_20)/df.mean_20) * 100
+  df['corpo_candle'] = ((df.close - df.open)/df.open) * 100
+  df['distancia_medias'] = ((df.mean_20 - df.mean_9)/df.mean_9) * 100
+
+  # teste de novas variáveis
+  df["V_Max"] = df["retorno"].rolling(15).max()
+  df["V_Min"] = df["retorno"].rolling(15).min()
+  df["I"] = df["retorno"].rolling(15).sum()
+
+
+  df.volatilidade = pd.qcut(df.volatilidade, 5, labels=False)
+  df.std_9 = pd.qcut(df.std_9, 5, labels=False)
+  df.std_20 = pd.qcut(df.std_20, 5, labels=False)
+  df.distancia_mean_9 = pd.qcut(df.distancia_mean_9, 5, labels=False)
+  df.distancia_mean_20 = pd.qcut(df.distancia_mean_20, 5, labels=False)
+  df.corpo_candle = pd.qcut(df.corpo_candle, 5, labels=False)
+  df.distancia_medias = pd.qcut(df.distancia_medias, 5, labels=False)
+
+  df['lag_5'] = df.close.shift(5)
+  df.dropna(inplace=True)
+  
+  for f in features:
+    for lag in range(1, lags + 1):
+      col = f'{f}_lag_{lag}'
+      df[col] = df[f].shift(lag)
+      cols.append(col)
+  
+  df['inclinacao_close'] = df.apply(lambda row: math.degrees(np.arctan((row['close'] - row['lag_5'])/5)), axis=1)
+
+  df['inclinacao_mma20'] = df.apply(lambda row: math.degrees(np.arctan((row['mma20'] - row['lag_5'])/5)), axis=1)
+
+  df['inclinacao_mme9'] = df.apply(lambda row: math.degrees(np.arctan((row['mme9'] - row['lag_5'])/5)), axis=1)
+
+  df.dropna(inplace=True)
+
+  if fit_model:
+    df['target'] = np.where(df.retorno > 0, 1, 0)
+    df.target = df.target.shift(-1)
+  
+  df.dropna(inplace=True)
+  return df, cols
+
+def cw(df):
+  c0, c1= np.bincount(df.target)
+  w0 = (1 / c0) * (len(df)) / 2
+  w1 = (1 / c1) * (len(df)) / 2
+  return {0: w0, 1: w1}
+
+# def cw(df):
+#   c0, c1, c2= np.bincount(df.target)
+#   w0 = (1 / c0) * (len(df)) / 3
+#   w1 = (1 / c1) * (len(df)) / 3
+#   w2 = (1 / c2) * (len(df)) / 3
+#   return {0: w0, 1: w1, 2:w2}
+
+def set_seeds(seed=42):
+  random.seed(seed)
+  np.random.seed(seed)
+  tf.random.set_seed(seed)
+
+def create_model(hl=3, hu=200, cols=1, optimizer=Adam(learning_rate=0.001)):
+  model = keras.models.Sequential()
+  model.add(
+    keras.layers.Dense(hu, input_dim=len(cols), activation='relu')
+  )
+  for _ in range(hl):
+    model.add(
+      keras.layers.Dense(hu, activation='relu')
+    )
+    model.add(keras.layers.Dense(1, activation='sigmoid'))
+    model.compile(
+      loss='binary_crossentropy',
+      optimizer=optimizer,
+      metrics=['accuracy']
+    )
+  return model
+
+def create_ensemble_model(df):
+
+  sgd = SGDClassifier(
+    max_iter=5000, 
+    tol=1e-3, 
+    class_weight=cw(df),
+    validation_fraction=0.2,
+    n_jobs=-1,
+    shuffle=False,
+    verbose=1
+  )
+
+  random_forest = RandomForestClassifier(
+    n_estimators=500,
+    max_depth=3,
+    min_samples_split=50,
+    n_jobs=-1,
+    random_state=42,
+    class_weight=cw(df),
+    max_leaf_nodes=5,
+    verbose=1
+  )
+
+  logistic_regression = LogisticRegression(
+    class_weight=cw(df),
+    random_state=42,
+    max_iter=500,
+    multi_class='ovr',
+    n_jobs=-1,
+    verbose=1
+  )
+
+  bagg_sgd = BaggingClassifier(
+    sgd,
+    n_estimators=100,
+    bootstrap=True,
+    max_samples=0.80,
+    n_jobs=-1,
+    random_state=42,
+    verbose=1
+  )
+
+  bagg_random_forest = BaggingClassifier(
+    random_forest,
+    n_estimators=100,
+    bootstrap=True,
+    max_samples=0.80,
+    n_jobs=-1,
+    random_state=42,
+    verbose=1
+  )
+
+  bagg_logistic_regression = BaggingClassifier(
+    logistic_regression,
+    n_estimators=100,
+    bootstrap=True,
+    max_samples=0.80,
+    n_jobs=-1,
+    random_state=42,
+    verbose=1
+  )
+
+  clf = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=0.99),
+    StackingClassifier(
+      estimators=[
+        ('Bagg SGD', bagg_sgd),
+        ('Bagg Random Forest', bagg_random_forest),
+        ('Bagg Logistic regression', bagg_logistic_regression),
+        # ('SGD', sgd),
+        # ('Random Forest', random_forest),
+        # ('Logistic regression', logistic_regression)
+      ],
+      n_jobs=-1,
+      cv=3,
+      passthrough=True,
+      verbose=1,
+      final_estimator=logistic_regression
+    )
+  )
+  return clf
+
+def sgd_model(df):
+  sgd = SGDClassifier(
+    max_iter=5000, 
+    tol=1e-3, 
+    class_weight=cw(df),
+    validation_fraction=0.2,
+    n_jobs=-1,
+    shuffle=False,
+    verbose=1
+  )
+
+  clf = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=0.99),
+    sgd
+  )
+  return clf
+
+def ensemble_sgd_model(df):
+  sgd = SGDClassifier(
+    max_iter=5000, 
+    tol=1e-3, 
+    class_weight=cw(df),
+    validation_fraction=0.2,
+    n_jobs=-1,
+    shuffle=False,
+    verbose=1
+  )
+
+  bagg_sgd = BaggingClassifier(
+    sgd,
+    n_estimators=100,
+    bootstrap=True,
+    max_samples=0.50,
+    n_jobs=-1,
+    random_state=42,
+    verbose=1
+  )
+
+  clf = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=0.99),
+    bagg_sgd
+  )
+  return clf
+
+
+def random_forest_model(df):
+  random_forest = RandomForestClassifier(
+    n_estimators=500,
+    max_depth=3,
+    min_samples_split=50,
+    n_jobs=-1,
+    random_state=42,
+    class_weight=cw(df),
+    max_leaf_nodes=5,
+    verbose=1
+  )
+
+  clf = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=0.99),
+    random_forest
+  )
+  return clf
+
+def logistic_regression_model(df):
+  logistic_regression = LogisticRegression(
+    class_weight=cw(df),
+    random_state=42,
+    max_iter=500,
+    n_jobs=-1,
+    verbose=1
+  )
+
+  clf = make_pipeline(
+    StandardScaler(),
+    PCA(n_components=0.99),
+    OneVsOneClassifier(logistic_regression, n_jobs=-1)
+  )
+  return clf
+
+# Envia ordem para o Metatrader
+def send_order(symbol = 'WING22', lot=1.0, deviation=10, order_type='buy'):
+  symbol_info = mt5.symbol_info(symbol)
+  if symbol_info is None:
+      print(symbol, "not found, can not call order_check()")
+      mt5.shutdown()
+      quit()
+  
+  # se o símbolo não estiver disponível no MarketWatch, adicionamo-lo
+  if not symbol_info.visible:
+      print(symbol, "is not visible, trying to switch on")
+      if not mt5.symbol_select(symbol,True):
+          print("symbol_select({}}) failed, exit",symbol)
+          mt5.shutdown()
+          quit()
+  
+  positions=mt5.positions_get(symbol=symbol)
+  # point = mt5.symbol_info(symbol).point
+  # print(positions== ())
+
+  if order_type == 'buy':
+    price = mt5.symbol_info_tick(symbol).ask
+    print('compra -> ' + str(price))
+    request = {
+      "action": mt5.TRADE_ACTION_DEAL,
+      "symbol": symbol,
+      "volume": lot,
+      "type": mt5.ORDER_TYPE_BUY,
+      "price": price,
+      "deviation": deviation,
+      "magic": 42,
+      "comment": "python script open",
+      "type_time": mt5.ORDER_TIME_GTC,
+      "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    result=mt5.order_send(request)
+    if result is None:
+        print("order_send() failed, error code =",mt5.last_error())
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+      print("2. order_send failed, retcode={}".format(result.retcode))
+    return result
+  elif order_type == 'sell':
+    price=mt5.symbol_info_tick(symbol).bid
+    print('venda -> ' + str(price))
+    request = {
+      "action": mt5.TRADE_ACTION_DEAL,
+      "symbol": symbol,
+      "volume": lot,
+      "type": mt5.ORDER_TYPE_SELL,
+      "price": price,
+      "deviation": deviation,
+      "magic": 42,
+      "comment": "python script open",
+      "type_time": mt5.ORDER_TIME_GTC,
+      "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    result=mt5.order_send(request)
+    if result is None:
+        print("order_send() failed, error code =",mt5.last_error())
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+      print("2. order_send failed, retcode={}".format(result.retcode))
+    return result
+  elif positions != () and positions[0][5] == 0:
+    price=mt5.symbol_info_tick(symbol).bid
+    request={
+      "action": mt5.TRADE_ACTION_DEAL,
+      "symbol": symbol,
+      "volume": lot,
+      "type": mt5.ORDER_TYPE_SELL,
+      "position": positions[0][0],
+      "price": price,
+      "deviation": deviation,
+      "magic": 42,
+      "comment": "python script close",
+      "type_time": mt5.ORDER_TIME_GTC,
+      "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    result=mt5.order_send(request)
+    result = ''
+    return result
+  elif positions != () and positions[0][5] == 1:
+    price=mt5.symbol_info_tick(symbol).ask
+    request={
+      "action": mt5.TRADE_ACTION_DEAL,
+      "symbol": symbol,
+      "volume": lot,
+      "type": mt5.ORDER_TYPE_BUY,
+      "position": positions[0][0],
+      "price": price,
+      "deviation": deviation,
+      "magic": 42,
+      "comment": "python script close",
+      "type_time": mt5.ORDER_TIME_GTC,
+      "type_filling": mt5.ORDER_FILLING_RETURN,
+    }
+    result=mt5.order_send(request)
+    result =''
+    return result
---
+++ main.py
@@ -0,0 +1,293 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import MetaTrader5 as mt5
+import datetime as dt
+import auxs
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_roc_curve
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+import math
+import pickle
+
+if not mt5.initialize():
+    print("initialize() failed")
+    mt5.shutdown()
+
+utc_from = dt.datetime.now() - dt.timedelta(9999)
+utc_to = dt.datetime.now()
+
+ativo = 'WIN@'
+
+rates = mt5.copy_rates_range(ativo, mt5.TIMEFRAME_M5, utc_from, utc_to)
+
+mt5.shutdown()
+
+df = pd.DataFrame(rates)
+df['time']=pd.to_datetime(df['time'], unit='s')
+
+df, cols = auxs.add_lags(df, 5)
+
+auxs.set_seeds()
+
+split = int(df.shape[0] * 0.8)
+
+train = df.iloc[:split].copy()
+test = df.iloc[split:].copy()
+
+test.index.name = "Data"
+test.reset_index(inplace = True)
+
+test["Data"] = pd.to_datetime(test["time"])
+
+# clf = auxs.create_ensemble_model(train)
+
+clf = auxs.sgd_model(train)
+# clf = auxs.ensemble_sgd_model(train)
+
+clf.fit(train[cols], train.target)
+
+# pickle.dump(clf, open('./models/' + ativo + '.sav', 'wb'))
+
+# clf = pickle.load(open('./models/' + ativo + '.sav', 'rb'))
+
+train['pred'] = clf.predict(train[cols])
+test['pred'] = clf.predict(test[cols])
+
+plt.figure()
+print('Matrix do teste')
+plot_confusion_matrix(clf, train[cols], train.target)
+
+plt.figure()
+print('Matrix do treino')
+plot_confusion_matrix(clf, test[cols], test.target)
+
+# test.pred = test.pred.shift()
+# test.dropna(inplace=True)
+
+venda = -test.retorno
+compra = test.retorno
+
+# test['lag_5'] = test.close.shift(5)
+# test.dropna(inplace=True)
+
+test['pode_operar'] = test.apply(lambda row: row['Data'].hour < 17, axis=1)
+
+regra_inclinacao = 'inclinacao_close'
+inclinacao = 40
+
+test['trade'] = np.where(
+    (test.pred == 0) & 
+    (test.mean_9 < test.mean_20) &
+    (test.close < test.mean_9) &
+    (test.close < test.mean_20) &
+    (test.pode_operar) &
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1) &
+        (test.mean_9 > test.mean_20) &
+        (test.close > test.mean_9) &
+        (test.close > test.mean_20) &
+        (test.pode_operar) &
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+
+test['trade2'] = np.where(
+    (test.pred == 0)  &
+    (test.pode_operar)&
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1)  &
+        (test.pode_operar)&
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+test['trade2_'] = np.where(
+    (test.pred == 0)  &
+    (test.pode_operar), venda,
+    np.where(
+        (test.pred == 1)  &
+        (test.pode_operar), compra, 0)
+)
+
+test['trade3'] = np.where(
+    (test.pred == 0) & 
+    (test.mean_9 < test.mean_20) &
+    (test.pode_operar)&
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1) &
+        (test.mean_9 > test.mean_20) &
+        (test.pode_operar)&
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+test['trade4'] = np.where(
+    (test.pred == 0) & 
+    (test.mean_9 < test.mean_20) &
+    (test.mean_20 < test.mean_20.shift()) &
+    (test.pode_operar)&
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1) &
+        (test.mean_9 > test.mean_20) &
+        (test.mean_20 > test.mean_20.shift()) &
+        (test.pode_operar)&
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+test['trade5'] = np.where(
+    (test.pred == 0) & 
+    (test.mean_9 < test.mean_20) &
+    (test.close < test.mean_9) &
+    (test.close < test.mean_20) &
+    (test.mean_20 < test.mean_20.shift()) &
+    (test.pode_operar)&
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1) &
+        (test.mean_9 > test.mean_20) &
+        (test.close > test.mean_9) &
+        (test.close > test.mean_20) &
+        (test.mean_20 > test.mean_20.shift()) &
+        (test.pode_operar)&
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+test['trade6'] = np.where(
+    (test.pred == 0) & 
+    (test.mean_9 < test.mean_20) &
+    (test.close < test.mean_9) &
+    (test.close < test.mean_20) &
+    (test.mean_20 < test.mean_20.shift()) &
+    (test.pode_operar)&
+    (test[regra_inclinacao] < -inclinacao), venda,
+    np.where(
+        (test.pred == 1) &
+        (test.mean_9 > test.mean_20) &
+        (test.close > test.mean_9) &
+        (test.close > test.mean_20) &
+        (test.mean_20 > test.mean_20.shift()) &
+        (test.pode_operar)&
+        (test[regra_inclinacao] > inclinacao), compra, 0)
+)
+
+# test['coloracao'] = np.where(
+#     (test.pred == 0) & 
+#     (test.mean_9 < test.mean_20), 'VENDE',
+#     np.where(
+#         (test.pred == 1) &
+#         (test.mean_9 > test.mean_20), 'COMPRA', '--'
+#     )
+# )
+
+test['lucro'] = test.trade.cumsum()*100
+test['lucro2'] = test.trade2.cumsum()*100
+test['lucro2_'] = test.trade2_.cumsum()*100
+test['lucro3'] = test.trade3.cumsum()*100
+test['lucro4'] = test.trade4.cumsum()*100
+test['lucro5'] = test.trade5.cumsum()*100
+test['lucro6'] = test.trade6.cumsum()*100
+
+fig_size = (15,12)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 1')
+plt.plot(test.time, test.lucro)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 2')
+plt.plot(test.time, test.lucro2)
+plt.figure(figsize=fig_size)
+plt.title('Trade 2_')
+plt.plot(test.time, test.lucro2_)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 3')
+plt.plot(test.time, test.lucro3)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 4')
+plt.plot(test.time, test.lucro4)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 5')
+plt.plot(test.time, test.lucro5)
+
+plt.figure(figsize=fig_size)
+plt.title('Trade 6')
+plt.plot(test.time, test.lucro6)
+
+plot_roc_curve(clf, test[cols], test.target)
+
+plt.show()
+
+# daqui para baixo é do Leandro
+
+trade = 'trade2'
+
+test["train_test"] = np.where(test["Data"] > train.time[len(train)-1], 1, -1)
+
+base_agregada = test.resample("M", on = "Data").sum()
+
+base_agregada.loc[: , "Retorno_Modelo_Acumulado"] = base_agregada[trade].cumsum()*100
+
+summary = test.copy()
+summary["Data"] = pd.to_datetime(summary["time"], format = "%Y-%m")
+summary = summary.groupby([summary["Data"].dt.year]).agg({trade: sum})
+summary.index = summary.index.set_names(["Ano"])
+
+summary_days = test.copy()
+summary_days["Data"] = pd.to_datetime(summary_days["time"], format = "%Y-%m")
+summary_days = summary_days.groupby([summary_days["Data"].dt.year, summary_days["Data"].dt.month, summary_days["Data"].dt.day]).agg({trade: sum})
+summary_days.index = summary_days.index.set_names(['Ano','Mes',"Dias"])
+
+summary_mes = test.copy()
+summary_mes["Data"] = pd.to_datetime(summary_mes["time"], format = "%Y-%m")
+summary_mes = summary_mes.groupby([summary_mes["Data"].dt.year, summary_mes["Data"].dt.month]).agg({trade: sum})
+summary_mes.index = summary_mes.index.set_names(["Ano", "Mes"])
+
+# print("--- OMNP: Resultado Ensemble IND FUT de 2003 à 2021 ---")
+# print("")
+# print("Treinamento: 2003-2009")
+# print("Teste      : 2010-2021")
+# print("Sem uso de stop - custos operacionais não incluídos")
+
+print()
+print('Acc - treino')
+print(accuracy_score(train.target, train.pred))
+
+print()
+print('Acc - teste')
+print(accuracy_score(test.target, test.pred))
+
+print()
+print('Report')
+print(classification_report(test.target, test.pred))
+
+print("---------------------------------------------------")
+print("Pior retorno diário:     {} %".format(round(summary_days[trade].min(), 3)*100))
+print("Melhor retorno diário:   {} %".format(round(summary_days[trade].max(), 3)*100))
+print("Média ganho diário:      {} %".format(round(summary_days[trade].mean(), 2)*100))
+print("---------------------------------------------------")
+print("Pior retorno mensal:     {} %".format(round(summary_mes[trade].min(), 3)*100))
+print("Melhor retorno mensal:   {} %".format(round(summary_mes[trade].max(), 3)*100))
+print("Média ganho mensal:      {} %".format(round(summary_mes[trade].mean(), 2)*100))
+print("---------------------------------")
+print("Pior retorno anual:      {} %".format(round(summary[trade].min(), 3)*100))
+print("Melhor retorno anual:    {} %".format(round(summary[trade].max(), 3)*100))
+print("Média ganho anual:       {} %".format(round(summary[trade].mean(), 2)*100))
+print("---------------------------------")
+print("# Anos negativos:        {}".format((summary[trade] < 0).sum()))
+print("# Anos positivos:        {}".format((summary[trade] > 0).sum()))
+print("---------------------------------")
+print("# Meses negativos:       {}".format((summary_mes[trade] < 0).sum()))
+print("# Meses positivos:       {}".format((summary_mes[trade] > 0).sum()))
+print("---------------------------------")
+print("# Dias negativos:       {}".format((summary_days[trade] < 0).sum()))
+print("# Dias positivos:       {}".format((summary_days[trade] > 0).sum()))
+print("---------------------------------")
+print("# Total(simples):        {} %".format(round(summary_mes[trade].sum(), 3)*100))
